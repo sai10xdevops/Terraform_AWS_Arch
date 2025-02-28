cc20dd7398efa814362da7f503b66d35a61ccca1
Surapaneni Sai Shanmukkha <90093621+SaiShanmukkha@users.noreply.github.com>
2025-02-28 13:10:24 -0600

    launch ec2_instance

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
-provider "aws" {
-  region = "ap-south-1"
+resource "aws_instance" "web" {
+  ami           = "ami-05b5cad4abb7f9a27"
+  instance_type = "t2.micro"
+
+  tags = {
+    "Name" = "TF_Dummy"
+  }
 }
